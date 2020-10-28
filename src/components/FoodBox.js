e11da3d5d16e0e3ecac26a59c65333fc6fcb1f26
Claire Lacanal <lacanal.claire@gmail.com>
2020-10-28 23:10:25 +0100

    ite 1 and 2 done

--- src/components/FoodBox.js
+++ src/components/FoodBox.js
@@ -1,13 +1,45 @@
-import { render } from '@testing-library/react';
-import React from 'react';
 
-const FoodBox = (props) => {
-    render(
-        <div>
-            <p>{props.content.p}</p>
+import React from 'react';
+import foods from '../foods.json';
+
+
+class FoodBox extends React.Component {
+
+    render() {
+        return(
+            <div className="box">
+  <article className="media">
+    <div className="media-left">
+      <figure className="image is-64x64">
+        <img src={this.props.image}/>
+      </figure>
+    </div>
+    <div className="media-content">
+      <div className="content">
+        <p>
+          <strong>{this.props.name}</strong> <br />
+          <small>{this.props.calories} cal</small>
+        </p>
+      </div>
+    </div>
+    <div className="media-right">
+      <div className="field has-addons">
+        <div className="control">
+          <input className="input" type="number" value={this.props.quantity} />
         </div>
+        <div className="control">
+          <button className="button is-info">
+            +
+          </button>
+        </div>
+      </div>
+    </div>
+  </article>
+</div>
         )
-       
+    }
+
+
 }
 
 export default FoodBox;
